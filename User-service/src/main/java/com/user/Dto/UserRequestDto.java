36f0794be4fdb71ec3336bf30291404d242e4ffb
Pradip-sanap <pradipsanap161@gmail.com>
2026-07-04 15:18:01 +0530

    Small change: Validation for email and phone in UserRequestDto

--- User-service/src/main/java/com/user/Dto/UserRequestDto.java
+++ User-service/src/main/java/com/user/Dto/UserRequestDto.java
@@ -1,8 +1,10 @@
 package com.user.Dto;
 
 import com.user.contants.Role;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,9 +24,11 @@ public class UserRequestDto {
     private String lastName;
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number" )
     private String phone;
 
     @NotNull(message = "Role is required")
